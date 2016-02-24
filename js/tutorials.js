d5b028e7b141faedd1e2c451b6bdd0cd5483884e
kbrito <airnomadkai@gmail.com>
2016-02-24 15:31:53 -0600

    Activity 05 Assignment
    In progress. This is late. I can't seem to get out of this pit of
    turning things in late... :/

--- js/tutorials.js
+++ js/tutorials.js
@@ -7,7 +7,10 @@
 
 
 // This piece sets the initial view of the map upon openning the map
-var map = L.map('map').setView([51.505, -0.09], 13);
+
+var map = L.map('map').setView([33.35, -84.5718
+], 4);
+
 
 //add tile layer...replace project id and accessToken with your own
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
@@ -18,24 +21,127 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
 			id: 'mapbox.streets'
 		}).addTo(map);
 
+
+/* Map of GeoJSON data from MegaCities.geojson */
+
+
+    //Example 2.3 line 22...load the data
+    $.ajax("data/AirportDataEdit.geojson", {
+        dataType: "json",
+        success: function(response){
+            //create marker options
+            var geojsonMarkerOptions = {
+                radius: 8,
+                fillColor: "#0A2869",
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            };
+
+            var attribute = "CY14_Enplanements";
+
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(response, {
+                pointToLayer: function (feature, latlng){
+
+                    //Step 5: For each feature, determine its value for the selected attribute
+                    var attValue = Number(feature.properties[attribute]);
+
+                    //examine the attribute value to check that it is correct
+                    console.log(feature.properties, attValue);
+
+                    geojsonMarkerOptions.radius = calcPropRadius(attValue);
+
+                    return L.circleMarker(latlng, geojsonMarkerOptions);
+                }
+            }).addTo(map);
+        }
+    });
+
+function calcPropRadius(attValue) {
+    //scale factor to adjust symbol size evenly
+    var scaleFactor = 0.00005;
+    //area based on attribute value and scale factor
+    var area = attValue * scaleFactor;
+    //radius calculated based on area
+    var radius = Math.sqrt(area/Math.PI);
+
+    return radius;
+};
+
+
+//Step 3: Add circle markers for point features to the map
+function createPropSymbols(data, map){
+    //create marker options
+    var geojsonMarkerOptions = {
+        radius: 8,
+        fillColor: "#0A2869",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    //Step 4: Determine which attribute to visualize with proportional symbols
+    var attribute = "CY14_Enplanements";
+
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
+
+            //Step 5: For each feature, determine its value for the selected attribute
+            var attValue = Number(feature.properties[attribute]);
+
+            //examine the attribute value to check that it is correct
+            console.log(feature.properties, attValue);
+            
+
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        }
+    }).addTo(map);
+};
+
+//Step 2: Import GeoJSON data
+function getData(map){
+    //load the data
+    $.ajax("data/MegaCities.geojson", {
+        dataType: "json",
+        success: function(response){
+            //call function to create proportional symbols
+            createPropSymbols(response, map);
+        }
+    });
+};
+
+
 // as it states, this code places a marker at the specified coordinates
+
+/*
 var marker = L.marker([51.5, -0.09]).addTo(map);
 
+
 // as it states, this code places a circle polygon at the specified coordinates 
+
 var circle = L.circle([51.508, -0.11], 500, {
     color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.5
 }).addTo(map);
 
+
 // as it states, this code places a general polygon at the specified coordinates
+
 var polygon = L.polygon([
     [51.509, -0.08],
     [51.503, -0.06],
     [51.51, -0.047]
 ]).addTo(map);
 
+
+
 // as it states, this code places a general popup bubble upon interactive click
+
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 circle.bindPopup("I am a circle.");
 polygon.bindPopup("I am a polygon.");
@@ -47,6 +153,7 @@ var popup = L.popup()
     .openOn(map);
 
 
+
 // this code enables the click features on the map
 function onMapClick(e) {
     popup
@@ -55,79 +162,7 @@ function onMapClick(e) {
         .openOn(map);
 }
 
+
 map.on('click', onMapClick);
+*/
 
-
-
-/* Map of GeoJSON data from MegaCities.geojson */
-
-//function to instantiate the Leaflet map
-function createMap(){
-    //create the map
-    var map = L.map('map', {
-        center: [20, 0],
-        zoom: 2
-    });
-
-    //add OSM base tilelayer
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-    }).addTo(map);
-
-    //call getData function
-    getData(map);
-};
-
-//function to retrieve the data and place it on the map
-function getData(map){
-    //load the data
-    $.ajax("data/MegaCities.geojson", {
-        dataType: "json",
-        success: function(response){
-
-        	//create marker options
-            var geojsonMarkerOptions = {
-                radius: 8,
-                fillColor: "#ff7800",
-                color: "#000",
-                weight: 1,
-                opacity: 1,
-                fillOpacity: 0.8
-            };
-
-            //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(response, {
-                pointToLayer: function (feature, latlng){
-                    return L.circleMarker(latlng, geojsonMarkerOptions);
-                }
-            }).addTo(map);
-
-        }
-    });
-};
-
-    //Example 2.3 line 22...load the data
-    $.ajax("data/MegaCities.geojson", {
-        dataType: "json",
-        success: function(response){
-            //create marker options
-            var geojsonMarkerOptions = {
-                radius: 8,
-                fillColor: "#ff7800",
-                color: "#000",
-                weight: 1,
-                opacity: 1,
-                fillOpacity: 0.8
-            };
-
-            //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(response, {
-                pointToLayer: function (feature, latlng){
-                    return L.circleMarker(latlng, geojsonMarkerOptions);
-                }
-            }).addTo(map);
-        }
-    });
-
-
-$(document).ready(createMap);
